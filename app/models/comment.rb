dad6c32330db8406a5f4ecbca399de6e1a543599
Yusra A. <84106371+CodeX-y@users.noreply.github.com>
2024-06-20 20:46:46 +0000

    fixed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,9 +12,9 @@
 
 class Comment < ApplicationRecord
   validates(:commenter, { :presence => true })
-
+  
   def commenter
-    my_id = self.id
+    my_id = self.author_id
     
     matching_users = User.where({ :id => my_id })
 
